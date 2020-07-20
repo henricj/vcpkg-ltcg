e1ebd3ded229486e96bbdb573adf9f7346359911
Henric Jungheim <software@henric.org>
2020-07-20 13:41:30 -0700

    Disable SDL2's dynapi.

--- ports/sdl2/portfile.cmake
+++ ports/sdl2/portfile.cmake
@@ -7,6 +7,7 @@ vcpkg_from_github(
     PATCHES
         deps.patch
         alsa-dep-fix.patch
+        disable-dynapi.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" SDL_STATIC)
